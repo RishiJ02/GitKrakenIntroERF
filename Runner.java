87ff78c232410b95164452bfc7dea1fec2bf5fdf
Evelyn Fu <evelynfu15@gmail.com>
2019-06-07 09:03:29 -0400

    PartnerB added comments about what he/she learned

--- Runner.java
+++ Runner.java
@@ -6,6 +6,8 @@ public class Runner{
 			System.out.println("yes");
 		else System.out.println("no");
 
+		//I learned how to make a lamda function in java and have it return a value. I also learned how to correctly stage changes and push and pull in gitkraken. Also, I learned how to use branches and merge them in gitkraken.
+
 			float x1f = 10;
 			float x2f = 1;
 			Quotient lobj = (float x1, float x2)->System.out.printf("%.3f",x1/x2);
