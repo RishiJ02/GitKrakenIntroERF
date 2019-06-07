3f69270e2e5609e8e39946c036fb0b3bc87f668f
Rishi <rishijammla@gmail.com>
2019-06-07 08:45:35 -0400

    Runner and interface for printing quotient functionality created

--- Runner.java
+++ Runner.java
@@ -1,4 +1,12 @@
 public class Runner{
 	public static void main(String[]args){
+			float x1f = 10;
+			float x2f = 1;
+			Quotient lobj = (float x1, float x2)->System.out.printf("%.3f",x1/x2);
+			lobj.getQuotient(x1f,x2f);
+		}
+	}
+	public interface Quotient{
+					public void getQuotient(float x1, float x2);
 	}
 };
