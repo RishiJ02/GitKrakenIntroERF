16a8182566b9df59c431f5d5b88fefa5a41489e3
Evelyn Fu <evelynfu15@gmail.com>
2019-06-07 08:41:01 -0400

    Runner and interface for perfect square functionality created

--- Runner.java
+++ Runner.java
@@ -1,4 +1,13 @@
 public class Runner{
 	public static void main(String[]args){
+		PerfectSquare psobj = (int x)->{return (((double)Math.sqrt((double)x) % 1.0) == 0.0);};
+
+		if(psobj.perfectSquare(16))
+			System.out.println("yes");
+		else System.out.println("no");
 	}
-};
+}
+
+interface PerfectSquare{
+	boolean perfectSquare(int x);
+}
