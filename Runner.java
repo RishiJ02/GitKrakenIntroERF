39296a6be0ebac9f613a17ff1689982bb5e0051e
Evelyn Fu <evelynfu15@gmail.com>
2019-06-07 09:01:22 -0400

    Combined all work into one single file

--- Runner.java
+++ Runner.java
@@ -1,24 +1,20 @@
 public class Runner{
 	public static void main(String[]args){
-<<<<<<< HEAD
 		PerfectSquare psobj = (int x)->{return (((double)Math.sqrt((double)x) % 1.0) == 0.0);};
 
 		if(psobj.perfectSquare(16))
 			System.out.println("yes");
 		else System.out.println("no");
-=======
+
 			float x1f = 10;
 			float x2f = 1;
 			Quotient lobj = (float x1, float x2)->System.out.printf("%.3f",x1/x2);
 			lobj.getQuotient(x1f,x2f);
 		}
-	}
 	public interface Quotient{
-					public void getQuotient(float x1, float x2);
->>>>>>> Print_Quotient
+		public void getQuotient(float x1, float x2);
 	}
-}
-
-interface PerfectSquare{
-	boolean perfectSquare(int x);
+	interface PerfectSquare{
+		boolean perfectSquare(int x);
+	}
 }
